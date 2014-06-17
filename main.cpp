57933a86c9215193498b46645ba6da400c233185
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-06-17 03:57:55 -0400

    Added the timer ISR.
    Added a bunch of defines for variables that will be used throughout the
    program.
    
    The timer ISR is initalized using TCCR* registers. Then, the OCR1A
    register is configured so the ISR will fire at certain time intervals.

--- main.cpp
+++ main.cpp
@@ -1,88 +1,184 @@
+//Author: Ryan - David Reyes
+/*
+ * The motor gear ratio is 30
+ * The encoder resolution is 64
+ * That makes 1920 ticks per shaft rev.
+ *
+ * The timer interrupt uses the following formula
+ *
+ * Motor Pinout:
+ * Red    | motor power (connects to one motor terminal)
+ * Black  | motor power (connects to the other motor terminal)
+ * Green  | encoder GND
+ * Blue   | encoder Vcc (3.5 - 20 V)
+ * Yellow | encoder A output
+ * White  | encoder B output
+ *
+ * (16M/prescaler)/(desired frequency) = number of counts for CTC mode.
+ */
+#define ARDUINO 102
 #include <math.h>
 #include <Arduino.h>
-#include "encoder.h"
+#include "Encoder.h"
+#include "PID.h"
 
-#define PWM_PINOUT 4
+#define NO_PRESCALING 0x01
+#define PRESCALE_8    0x02
+#define PRESCALE_64   0x03
+
+#define KP 1.0
+#define KI 0
+#define KD 0
+//this is the number of ticks for CTC mode
+#define CTC_MATCH 16000 //*should* run the interrupt at 1kHz
+
+//Motor 0 Pins
+#define MOTOR0_PWM_PINOUT 5
+#define MOTOR0_DIRECTIONA 24
+#define MOTOR0_DIRECTIONB 25
+
+//Motor 1 Pins
+#define MOTOR1_PWM_PINOUT 2
 #define MOTOR1_DIRECTIONA 53
 #define MOTOR1_DIRECTIONB 52
+
+//Motor 2 Pins
+#define MOTOR2_PWM_PINOUT 3
+#define MOTOR2_DIRECTIONA 40
+#define MOTOR2_DIRECTIONB 41
+
+//Motor 3 Pins
+#define MOTOR3_PWM_PINOUT 4
+#define MOTOR3_DIRECTIONA 22
+#define MOTOR3_DIRECTIONB 23
+
+#define ENCODER_RESOLUTION
+#define GEAR_RATIO
 
 #define ENCODER1_A 2
 #define ENCODER1_B 3
 
+#define ARROW_UP    65
+#define ARROW_DOWN  66
+#define ARROW_LEFT  67
+#define ARROW_RIGHT 68
 
-void setup()
-{
-  pinMode(PWM_PINOUT, OUTPUT);
-  pinMode(MOTOR1_DIRECTIONA, OUTPUT);
+//structures
+struct motor {
+  char  pwm;
+  char  pwm_pin;
+  char  directiona;
+  char  directionb;
+  float command_velocity; //specified in rad/s
+  float current_velocity; //specified in rad/s
+};
 
-  digitalWrite(MOTOR1_DIRECTIONA,LOW);
-  digitalWrite(MOTOR1_DIRECTIONB,LOW);
+//variables
+motor    motor0;
+pid_data motor0_pid_data;
+Encoder  motor0_encoder(
 
-  Serial.begin(9600);
+//function prototypes
+void setup();
+void readKeyboard();
+void timerInterrupt();
 
-  encoder::initEncoderChannelA(ENCODER1_A);
-  encoder::initEncoderChannelB(ENCODER1_B);
+//interrupt handler for the timer compare
+ISR(TIMER1_COMPA_vect) {
+
+  fixedUpdatePID(motor0_pid_data, current_error);
 }
 
-int main()
-{
-  char duty = 50;
-  int counter = 0;
-  int incomingByte = 0;
-  unsigned long prev_enc_value = 1;
-
+//main
+int main() {
   init();
   setup();
 
-  while (1)
-  {
-    analogWrite(PWM_PINOUT, duty);
-
-    if (Serial.available() > 0)
-    {
-      incomingByte = Serial.read();
-
-      if (incomingByte == ' ')
-      {
-        if (counter % 2 == 1)
-        {
-          digitalWrite(MOTOR1_DIRECTIONB,LOW);
-          digitalWrite(MOTOR1_DIRECTIONA,LOW);
-        }
-        else
-        {
-          digitalWrite(MOTOR1_DIRECTIONA,LOW);
-          digitalWrite(MOTOR1_DIRECTIONB,HIGH);
-        }
-        counter ++;
-      }
-
-      if (incomingByte == 65)
-        duty += 2;
-      else if (incomingByte == 66)
-        duty -= 2;
-
-      else if (incomingByte == 67)
-      {
-        digitalWrite(MOTOR1_DIRECTIONA,LOW);
-        digitalWrite(MOTOR1_DIRECTIONB,HIGH);
-        counter = 1;
-      }
-
-      else if (incomingByte == 68)
-      {
-        digitalWrite(MOTOR1_DIRECTIONB,LOW);
-        digitalWrite(MOTOR1_DIRECTIONA,HIGH);
-        counter = 1; //reset the counter so space to stop works correctly.
-      }
-    }
-
-    if (encoder::encoder_count != prev_enc_value)
-    {
-      Serial.print(encoder::encoder_count, DEC);
-      Serial.print("\n");
-      prev_enc_value = encoder::encoder_count;
-    }
+  while (1) {
+    readKeyboard();
   }
   return 0;
-}
+} //end main()
+
+void setup() {
+  noInterrupts();
+  //set up the motors
+  motor0.pwm = 0;
+  motor0.pwm_pin = MOTOR0_PWM_PINOUT;
+  motor0.directiona = MOTOR0_DIRECTIONA;
+  motor0.directionb = MOTOR0_DIRECTIONB;
+  motor0.command_velocity = 0;
+  motor0.current_velocity = 0;
+
+  //set all the pins to output
+  pinMode(MOTOR0_PWM_PINOUT, OUTPUT);
+  pinMode(MOTOR0_DIRECTIONA, OUTPUT);
+  pinMode(MOTOR0_DIRECTIONB, OUTPUT);
+
+  //stop the motor
+  digitalWrite(motor0.directiona,LOW);
+  digitalWrite(motor0.directionb,LOW);
+
+  //start the serial device
+  Serial.begin(9600);
+
+  //set the PID constants
+  setPIDConstants(motor0_pid_data, KP, KI, KD);
+
+  //configure the timer interrupt
+  TCCR1A = 0;
+  TCCR1B = NO_PRESCALING; //sets the prescaler to 1
+  TCNT1  = 0;             //resets the timer
+
+  OCR1A = CTC_MATCH;
+  TCCR1B |= (0x01 << WGM12);  //enables CTC mode
+  TIMSK1 |= (0x01 << OCIE1A); //enables the interrupt CTC interrupt
+
+  interrupts();
+} //end setup()
+
+void readKeyboard() {
+  static int counter = 0;
+  int incomingByte = 0;
+
+  analogWrite(motor0.pwm_pin, duty);
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+
+    switch(incomingByte) {
+      case ' ':
+        if (counter % 2 == 1) {
+          digitalWrite(motor0.directiona,LOW);
+          digitalWrite(motor0.directionb,LOW);
+        }
+        else {
+          digitalWrite(motor0.directiona,LOW);
+          digitalWrite(motor0.directionb,HIGH);
+        }
+        counter ++;
+        break;
+
+      case ARROW_UP:
+        motor0.command_velocity += 0.2;
+        break;
+
+      case ARROW_DOWN:
+        motor0.command_velocity -= 0.2;
+        break;
+
+      case ARROW_LEFT:
+        digitalWrite(motor0.directiona,HIGH);
+        digitalWrite(motor0.directionb,LOW);
+        counter = 1;
+        break;
+
+      case ARROW_RIGHT:
+        digitalWrite(motor0.directiona,LOW);
+        digitalWrite(motor0.directionb,HIGH);
+        counter = 1; //reset the counter so space to stop works correctly.
+        break;
+      default:
+        break;
+    }
+  }
+} //end readKeyboard();
