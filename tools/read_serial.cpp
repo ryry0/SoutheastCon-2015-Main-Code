96e5715821bf5dd7aa3b81330175dc4b3f48aedb
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-09-12 12:44:59 -0400

    Updated read and write serial commands.
    They can now accept a path from cli arguments.

--- tools/read_serial.cpp
+++ tools/read_serial.cpp
@@ -3,11 +3,16 @@
 #include <stdio.h>
 
 char * port_name = "/dev/ttyACM0";
-int main()
+int main(int argc, char ** argv)
 {
   char output = 0;
-  int serial_port = open_port(port_name);
-  init_serial_port(serial_port);
+  int serial_port;
+  if (argc > 1)
+    port_name = argv[1];
+
+  serial_port = open_port(port_name);
+  if (init_serial_port(serial_port) == -1)
+    return 1;
 
   while (1)
   {
