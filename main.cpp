27715fc394ce13560d6fea31c767b72ee4c3b4f0
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2015-02-15 15:02:49 -0500

    Second Incremental fixes.
    Split keyboard functions into another file.

--- main.cpp
+++ main.cpp
@@ -112,11 +112,13 @@ pid_data motor_pid_data[NUM_MOTORS]; //struct of data dealing with PID
 volatile int motor_encoders[NUM_MOTORS];
 
 //function prototypes
+#ifdef KBD_DEBUG
+#include <kbd_dbg.h> //keyboard debugging function
+#endif
 void setup();
-void readKeyboard(movement_vector_t &movement_vector, states_t &state);
 bool readLineSensors(line_following_packet_t &line_packet);
 
-//velocity computation functions
+//velocity computation function
 float computeVelocity(const int wheelnum,
     const movement_vector_t &movement_vector);
 
@@ -175,9 +177,8 @@ int main() {
   unsigned long start_time = 0;
 
 #ifdef SERIAL_DEBUG
+  //These variables hold the previous values for debugging purposes
   /*
-     These variables hold the previous values for debugging
-     purposes
      float prev_motor_velocity = 0;
      long  prev_encoder_value = 0;
      */
@@ -295,7 +296,7 @@ int main() {
 
 #ifdef SERIAL_DEBUG
     if ((prev_state != robot_state) ||
-        //(prev_x_vel != movement_vector.x_velocity) ||
+        (prev_x_vel != movement_vector.x_velocity) ||
         (prev_y_vel != movement_vector.y_velocity) ||
         (prev_ang_vel != movement_vector.angular_velocity) ||
         (prev_back_sensors != back_sensors) ||
@@ -413,90 +414,6 @@ void setup() {
 
   interrupts();
 } //end setup()
-
-void readKeyboard(movement_vector_t &movement_vector, states_t &state) {
-  char incoming_byte = 0;
-
-  //read the serial
-  if (Serial.available() > 0) {
-    incoming_byte = Serial.read();
-
-    switch(incoming_byte) {
-      case ' ': //stop line following
-        movement_vector.x_velocity = 0;
-        movement_vector.y_velocity = 0;
-        movement_vector.angular_velocity = 0;
-        for (int i = 0; i < NUM_MOTORS; ++i) {
-          motors[i].pwm = 0;
-          motors[i].command_position = 0;
-          motors[i].command_velocity = 0;
-          motors[i].encoder_value = 0;
-          motor_encoders[i] = 0;
-          motor_pid_data[i].pid_output = 0;
-          motor_pid_data[i].previous_error = 0;
-          motor_pid_data[i].integral_error = 0;
-          stopMotor(motors[i]);
-        }
-        state = STOPPED;
-        break;
-
-      case 'F': //start
-        state = FOLLOW_LINE;
-        digitalWrite(RESET_PIN, LOW);
-        for (int i = 0; i < 100; ++i);
-        digitalWrite(RESET_PIN, HIGH); //HACK FIX: FIND BETTER SOLUTION
-        digitalWrite(RESET_PIN, LOW);
-        for (int i = 0; i < 100; ++i);
-        digitalWrite(RESET_PIN, HIGH);
-        break;
-
-      case 'D':
-        state = DBG_LINE_SENSORS;
-        digitalWrite(RESET_PIN, LOW);
-        for (int i = 0; i < 100; ++i);
-        digitalWrite(RESET_PIN, HIGH); //HACK FIX: FIND BETTER SOLUTION
-        digitalWrite(RESET_PIN, LOW);
-        for (int i = 0; i < 100; ++i);
-        digitalWrite(RESET_PIN, HIGH);
-        break;
-
-      case 'w':
-        movement_vector.x_velocity += 0.1;
-        break;
-
-      case 's':
-        movement_vector.x_velocity -= 0.1;
-        break;
-
-      case 'a':
-        movement_vector.y_velocity += 0.1;
-        break;
-
-      case 'd':
-        movement_vector.y_velocity -= 0.1;
-        break;
-
-      case 'e':
-        movement_vector.angular_velocity -= .24;//.3;
-        break;
-
-      case 'q':
-        movement_vector.angular_velocity += .24;//.3;
-        break;
-
-      case 'l':
-        digitalWrite(LED_PIN, LOW);
-        break;
-
-      case 'L':
-        digitalWrite(LED_PIN, HIGH);
-        break;
-
-      default:
-        break;
-    } //end switch
-  } //end if
-} //end readKeyboard();
 
 //this deals with reading the slave arduino's line sensors
 //readLineSensors returns whether or not the Request packet should be resent
